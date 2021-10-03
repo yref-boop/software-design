a8003da11eeef109bc98e7e52c28598eeaebdece
Ferni <a.fernandez9@udc.es>
2021-10-03 19:05:13 +0200

    method isPasswordSafe has been implemented

--- DS-61-08-A1/src/e1/StringCount.java
+++ DS-61-08-A1/src/e1/StringCount.java
@@ -9,6 +9,7 @@ public class StringCount {
     /**
      * Counts the number of words in a given String.
      * Words are groups of characters separated by one or more spaces.
+     *
      * @param text String with the words
      * @return Number of words in the String or zero if it is null
      */
@@ -24,40 +25,40 @@ public class StringCount {
         return words.length;
     }
 
-
-
     /**
      * Counts the number of times the given character appears in the String.
      * Accented characters are considered different characters.
+     *
      * @param text String with the characters
-     * @param c the character to be found
+     * @param c    the character to be found
      * @return Number of times the character appears in the String or zero if null
      */
-    public static int countChar(String text , char c) {
+
+    public static int countChar(String text, char c) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
         int count = 0;
 
-        for(int i = 0; i < text.length(); i++){
-            if(text.charAt(i) == c)
-                count ++;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == c)
+                count++;
         }
         return count;
     }
+
     /**
-     *
      * Counts the number of times the given character appears in the String.
      * The case is ignored so an ’a’ is equal to an ’A’.
      * Accented characters are considered different characters.
+     *
      * @param text String with the characters
-     * @param c the character to be found
+     * @param c    the character to be found
      * @return Number of times the character appears in the String or zero if null
      */
 
-
-    public static int countCharIgnoringCase(String text , char c) {
+    public static int countCharIgnoringCase(String text, char c) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
@@ -66,13 +67,12 @@ public class StringCount {
 
         int count = 0;
 
-        for(int i = 0; i < text_lc.length(); i++){
-            if(text_lc.charAt(i) == c)
-                count ++;
+        for (int i = 0; i < text_lc.length(); i++) {
+            if (text_lc.charAt(i) == c)
+                count++;
         }
         return count;
     }
-
 
 
     /**
@@ -82,8 +82,25 @@ public class StringCount {
      * - Has a lowercase character
      * - Has a digit
      * - Has a special character among these: ’?’, ’@’, ’#’, ’$’, ’.’ and ’,’
+     *
      * @param password The password , we assume it is not null.
      * @return true if the password is safe , false otherwise
      */
-    public static boolean isPasswordSafe(String password) { /* ... */ }
+
+    public static boolean isPasswordSafe(String password) {
+
+        boolean hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+        char ch;
+
+        for (int i = 0; i < password.length(); i++) {
+            ch = password.charAt(i);
+            if (Character.isLowerCase(ch)) hasLower = true;
+            if (Character.isUpperCase(ch)) hasUpper = true;
+            if (Character.isDigit(ch)) hasDigit = true;
+            hasSpecial = password.matches(".*[?@#$.,].*");
+        }
+
+        return (password.length() > 8) && hasUpper && hasLower && hasDigit && hasSpecial;
+    }
+
 }

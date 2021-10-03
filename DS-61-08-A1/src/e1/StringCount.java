bdc1ad886a613f764d26897fcf00c2eecafb35e2
yago <regofdez.yago@gmail.com>
2021-10-03 18:39:32 +0200

    String count class: countCharIgnoringClasses implementation

--- DS-61-08-A1/src/e1/StringCount.java
+++ DS-61-08-A1/src/e1/StringCount.java
@@ -1,5 +1,6 @@
-public class StringCount {
+package e1; //set the package of this class as e1
 
+public class StringCount {
 
     //attributes
     private String text;
@@ -12,6 +13,7 @@ public class StringCount {
      * @return Number of words in the String or zero if it is null
      */
 
+    //methods:
     public static int countWords(String text) {
 
         if (text == null || text.isEmpty()) {
@@ -56,48 +58,23 @@ public class StringCount {
 
 
     public static int countCharIgnoringCase(String text , char c) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
 
+        String text_lc = text.toLowerCase();
+
+        int count = 0;
+
+        for(int i = 0; i < text_lc.length(); i++){
+            if(text_lc.charAt(i) == c)
+                count ++;
+        }
+        return count;
     }
 
 
 
-    /**
-     * Checks if a password is safe according to the following rules:
-     * - Has at least 8 characters
-     * - Has an uppercase character
-     * - Has a lowercase character
-     * - Has a digit
-     * - Has a special character among these: ’?’, ’@’, ’#’, ’$’, ’.’ and ’,’
-     * @param password The password , we assume it is not null.
-     * @return true if the password is safe , false otherwise
-     */
-    public static boolean isPasswordSafe(String password) { /* ... */ }
-}public class StringCount {
-    /**
-     * Counts the number of words in a given String.
-     * Words are groups of characters separated by one or more spaces.
-     *
-     * @param text String with the words
-     * @return Number of words in the String or zero if it is null
-     */
-    public static int countWords(String text) { /* ... */ }
-    /**
-     * Counts the number of times the given character appears in the String.
-     * Accented characters are considered different characters.
-     * @param text String with the characters
-     * @param c the character to be found
-     * @return Number of times the character appears in the String or zero if null
-     */
-    public static int countChar(String text , char c) { /* ... */ }
-    /**
-     * Counts the number of times the given character appears in the String.
-     * The case is ignored so an ’a’ is equal to an ’A’.
-     * Accented characters are considered different characters.
-     * @param text String with the characters
-     * @param c the character to be found
-     * @return Number of times the character appears in the String or zero if null
-     */
-    public static int countCharIgnoringCase(String text , char c) { /* ... */ }
     /**
      * Checks if a password is safe according to the following rules:
      * - Has at least 8 characters

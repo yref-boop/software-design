fbc41adf3ffff1a80a433b0ab25a1917f25c9f81
yago <regofdez.yago@gmail.com>
2021-10-04 19:07:20 +0200

    Progressing work on Melody (e3): Most methods are done (to be checked later) except for hashCode and equals which are yet to be implemented
    Moreover, the test e3 has been added in order to check the correctness of the code (not included in this commit)

--- DS-61-08-A1/src/e3/Melody.java
+++ DS-61-08-A1/src/e3/Melody.java
@@ -1,26 +1,33 @@
 package e3;
 import java.util.*;
 
-
-
-
 public class Melody {
 
 //ATTRIBUTES:
 
     public enum Notes {DO, RE, MI, FA, SOL, LA, SI}
-    public enum Accidentals {SHARP, FLAT}
+    public enum Accidentals {NATURAL, SHARP, FLAT}
     float time;
 
+    public Notes Note;
+    public Accidentals Accidental;
 
 //METHODS:
+
+
+    //Constructor method:
+    public Melody(Notes Note, Accidentals Accidental, float time){
+        this.Note = Note;
+        this.Accidental = Accidental;
+        this.time = time;
+    }
+
+    List<Melody> list = new ArrayList<>();
 
     /**
      * Creates an empty Melody instance.
      */
-    public Melody () {
-        List<Melody> list = new ArrayList<Melody>();
-    }
+    public Melody () {List<Melody> list = new ArrayList<>();}
 
 
     /**
@@ -32,39 +39,58 @@ public class Melody {
      * or the time are not valid values.
      */
     public void addNote(Notes note, Accidentals accidental, float time) {
-        Notes readyStatus = Notes.valueOf("note");
+        list.add(new Melody(note, accidental, time));
     }
+
+
     /**
      * Returns the note on the given position
      * @param index The position of the note to get.
      * @return The note on index.
      * @throws IllegalArgumentException if the index is not a valid position.
      */
-    public Notes getNote(int index) { /* ... */ }
+    public Notes getNote(int index) {return(list.get(index)).Note;}
     /**
      * Returns the accidental of the note on the given position
      * @param index The position of the accidental to get.
      * @return The accidental on index.
      * @throws IllegalArgumentException if the index is not a valid position.
      */
-    public Accidentals getAccidental(int index) { /* ... */ }
+    public Accidentals getAccidental(int index) {return(list.get(index)).Accidental;}
     /**
      * Returns the duration of the note on the given position
      * @param index The position of the time to get.
      * @return The time on index.
      * @throws IllegalArgumentException if the index is not a valid position.
      */
-    public float getTime(int index) { /* ... */ }
+    public float getTime(int index) {return(list.get(index)).time;}
     /**
      * Returns the number of notes in this melody.
      * @return The number of notes in this melody.
      */
-    public int size() { /* ... */ }
+    public int size() {
+        int i = 0;
+        for (Melody melody : list){i++;}
+        return i;
+    }
+
+
     /**
      * Returns the duration of this melody.
      * @return The duration of this melody in milliseconds.
      */
-    public float getDuration () { /* ... */ }
+    public float getDuration () {
+
+        float total_time = 0;
+        int size = list.size();
+
+        for (Melody melody : list) {
+            total_time = total_time + melody.time;
+        }
+        return total_time;
+    }
+
+
     /**
      * Performs the equality tests of the current melody with another melody
      * passed as a parameter. Two melodies are equal if they represent the same
@@ -72,8 +98,10 @@ public class Melody {
      * @param o The melody to be compared with the current melody.
      * @return true if the melodies are equals , false otherwise.
      */
-    @Override
-    public boolean equals(Object o) { /* ... */ }
+  //  @Override
+  //  public boolean equals(Melody o) {}
+
+
     /**
      * Returns an integer that is a hash code representation of the melody.
      * Two melodies m1 , m2 that are equals (m1.equals(m2) == true) must
@@ -82,10 +110,30 @@ public class Melody {
      */
     @Override
     public int hashCode () { /* ... */ }
+
+
     /**
      * The string representation of this melody.
      * @return The String representantion of this melody.
      */
     @Override
-    public String toString () { /* ... */ }
+    public String toString () {
+
+        StringBuilder Melody_String = new StringBuilder();
+
+        for (Melody melody : list) {
+            Melody_String.append(melody.Note);
+        }
+
+        return Melody_String.toString();
+    }
+
+
+
+    public static void main(String args[]){
+    }
+
+
+
+
 }

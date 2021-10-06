5bd1bcee81b8ea3c80d02f0ac7c4bba29db7d44a
yago <regofdez.yago@gmail.com>
2021-10-06 19:44:50 +0200

    Finnished commenting e3:
    most functions have been commented
    with an special focus on explaining the if conditions on the equals() function

--- DS-61-08-A1/src/e3/Melody.java
+++ DS-61-08-A1/src/e3/Melody.java
@@ -41,7 +41,7 @@ public class Melody {
     public void addNote(Notes note, Accidentals accidental, float time) {
         if((note == null)||(accidental == null)||(time <= 0))
             throw new IllegalArgumentException("not valid parameters");
-        list.add(new Melody(note, accidental, time));
+        list.add(new Melody(note, accidental, time)); //adds a new note to the list
     }
 
 
@@ -54,7 +54,7 @@ public class Melody {
     public Notes getNote(int index) {
         if(index > list.size()-1)
             throw new IllegalArgumentException("index position not valid");
-        return(list.get(index)).Note;
+        return(list.get(index)).Note; //returns the note of the wanted object
     }
 
 
@@ -67,7 +67,7 @@ public class Melody {
     public Accidentals getAccidental(int index) {
         if(index > list.size())
             throw new IllegalArgumentException("index position not valid");
-        return(list.get(index)).Accidental;
+        return(list.get(index)).Accidental; //returns the accidental of the wanted object
     }
 
 
@@ -80,7 +80,7 @@ public class Melody {
     public float getTime(int index) {
         if(index > list.size())
             throw new IllegalArgumentException("index position not valid");
-        return(list.get(index)).time;
+        return(list.get(index)).time; //returns the time of the wanted object
     }
 
 
@@ -90,7 +90,7 @@ public class Melody {
      */
     public int size() {
         int i = 0;
-        for (Melody melody : list){i++;}
+        for (Melody melody : list){i++;} //for each item on melody, add 1 to i
         return i;
     }
 
@@ -104,7 +104,7 @@ public class Melody {
         float total_time = 0;
 
         for (Melody melody : list) {
-            total_time = total_time + melody.time;
+            total_time = total_time + melody.time; //for each item on the melody, add its time to total_time
         }
         return total_time;
     }
@@ -123,10 +123,16 @@ public class Melody {
         if (o == null){return false;}
         if (this.getClass() != o.getClass()){return false;}
 
-        for (int i = 0; i< list.size(); i++){
+        for (int i = 0; i< list.size(); i++){   //once for every item on the list
+
+            //two variables: o_mel and cur_mel (object melody and current melody respectively) are created
+            //in order to be used later
+            //they are not the lists but the items on the list (they change on each iteration of the for loop)
 
             Melody o_mel = ((Melody) o).list.get(i);
             Melody cur_mel = this.list.get(i);
+
+            //the following block of code checks that the parameters on each item are not null or invalid
 
             if(cur_mel.Note == null){return false;}
             if(o_mel.Note == null){return false;}
@@ -134,19 +140,29 @@ public class Melody {
             if(o_mel.Accidental == null){return false;}
             if(cur_mel.time <= 0){return false;}
             if(o_mel.time <= 0){return false;}
-            if(cur_mel.time != o_mel.time){return false;}
 
+            if(cur_mel.time != o_mel.time){return false;} //if the times are not equal, the items cannot be equal
+
+            //if all parameters are exactly the same, the objects certainly are equal
             if((cur_mel.time == o_mel.time)&&(cur_mel.Accidental == o_mel.Accidental)&&(cur_mel.Note==o_mel.Note)){continue;}
 
+
+            //the following code checks all possible conditions in which two items with different .Note cannot be equal
+            //it follows the table given in the statement of this practical
+
+            //fist, the cases of the notes MI FA, SI DO are checked since they behave differently than the others:
             if((((o_mel.Note == Notes.MI)&&(cur_mel.Note == Notes.FA))||((o_mel.Note == Notes.FA)&&(cur_mel.Note == Notes.MI))||
                 ((o_mel.Note == Notes.SI)&&(cur_mel.Note == Notes.DO))||((o_mel.Note == Notes.DO)&&(cur_mel.Note== Notes.SI)))){
 
+                    //this if basically states that if there is a MIb or MI accompanied by a FA# or FA, they cannot be equal
+                    //(the equality only holds for FA, MI# or MI, FAb, this if checks its negation in order to return false)
                     if((((o_mel.Accidental != Accidentals.SHARP)&&(o_mel.Note)== Notes.MI)||
                             ((cur_mel.Accidental != Accidentals. SHARP)&&(cur_mel.Note)==Notes.MI))&&
                             ((((o_mel.Accidental != Accidentals.FLAT)&&(o_mel.Note)== Notes.FA)||
                             ((cur_mel.Accidental != Accidentals. FLAT)&&(cur_mel.Note)==Notes.FA))))
                          return false;
 
+                    //this block of code is similar to the previous but analyzes the couple SI and DO
                     if((((o_mel.Accidental != Accidentals.SHARP)&&(o_mel.Note)== Notes.SI)||
                             ((cur_mel.Accidental != Accidentals. SHARP)&&(cur_mel.Note)==Notes.SI))&&
                             (((o_mel.Accidental != Accidentals.FLAT)&&(o_mel.Note)== Notes.DO)||
@@ -154,8 +170,20 @@ public class Melody {
                         return false;
 
             }else {
+                    //forall Notes different than MI FA, SI DO , in order to be equal, one need to have a # and another a b
+                    //in order to obtain the negation:
+                    // !(((b)&&(#))||(#)&&(b)) == (((!b)||(!#))&&((!#)||(!b)))
                     if (((o_mel.Accidental != Accidentals.FLAT) || (cur_mel.Accidental != Accidentals.SHARP)) &&
                             ((o_mel.Accidental != Accidentals.SHARP) || (cur_mel.Accidental != Accidentals.FLAT)))
+                        return false;
+
+                    //moreover, the difference between the two notes has to be of 1, else the cannot be equal
+                    // (SI DO case has already been studied before )
+                    //the ordinal can be used in this case since the enum of Notes is not going to be modified
+                    if((o_mel.Note.ordinal()-cur_mel.Note.ordinal() != 1)&&
+                    (o_mel.Note.ordinal()-cur_mel.Note.ordinal() != -1)&&
+                    (cur_mel.Note.ordinal()-o_mel.Note.ordinal() != -1)&&
+                    (cur_mel.Note.ordinal()-o_mel.Note.ordinal() != -1))
                         return false;
 
              }
@@ -173,7 +201,7 @@ public class Melody {
         int value = 1;
         Math.round(time);
 
-        for(int i = 0; i < note.length(); i++){
+        for(int i = 0; i < note.length(); i++){     //for each character, convert it to int and perform the operation
             value = 31 * value + (int)note.charAt(i);
         }
         value = 31 * value + Math.round(time);
@@ -193,12 +221,14 @@ public class Melody {
 
         int hash = 0;
 
-        for (Melody mel : this.list) {
+        for (Melody mel : this.list) { //for all elements on the list
 
-            if ((mel.Note == null)||(mel.Accidental == null)) {
+            if ((mel.Note == null)||(mel.Accidental == null)) { //if null, return 0 as hash
                 return hash;
             }
 
+            //the following switch selects the inputs of hash_aux following the data of each element,
+            // those that are equal will sennd the same input, thus obtaining the same hashCode
 
             switch (mel.Note) {
                 case DO -> {
@@ -284,27 +314,28 @@ public class Melody {
     }
 
 
-    /** 61597
+    /**
      * The string representation of this melody.
      * @return The String representation of this melody.
      */
     @Override
     public String toString () {
 
+        //StringBuilder is used
         int i = 1;
         StringBuilder Melody_String = new StringBuilder();
 
-        for (Melody melody : list) {
-            Melody_String.append(melody.Note);
+        for (Melody melody : list) { //forall elements on the list
+            Melody_String.append(melody.Note); //append the note
             switch (melody.Accidental) {
-                case FLAT -> Melody_String.append("b");
-                case SHARP -> Melody_String.append("#");
-                default -> {}
+                case FLAT -> Melody_String.append("b");     //if accidental is flat, append b
+                case SHARP -> Melody_String.append("#");    //if sharp, #
+                default -> {}                               //if natural, do nothing
             }
-            Melody_String.append("(");
-            Melody_String.append(melody.time);
+            Melody_String.append("("); //( needed
+            Melody_String.append(melody.time); //apend time
 
-            if(i< list.size()){ Melody_String.append(") ");}
+            if(i< list.size()){ Melody_String.append(") ");} //since there is no space on the last iteration, this is needed
             else {Melody_String.append(")");}
             i++;
         }

df8ca98dc947a1b343fd9cace821ca8a1e159333
yago <regofdez.yago@gmail.com>
2021-12-09 20:50:46 +0100

    started Test e2 + more cleaning-up

--- DS-61-08-DA/test/e2/TaskTest.java
+++ DS-61-08-DA/test/e2/TaskTest.java
@@ -1,4 +1,48 @@
 package e2;
 
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//adjacency list:
+
+
+//A -> B -> D
+//B -> E
+//C -> A -> F
+//D -> E
+//E ->
+//F -> E -> J
+//G -> H -> F
+//H -> J
+//J ->
+
 public class TaskTest {
+
+    // Add edge
+    static void addEdge(ArrayList<ArrayList<Integer>> am, int s, int d) {
+        am.get(s).add(d);
+        am.get(d).add(s);
+    }
+
+    @Test
+    void graph(){
+
+        // Create the graph
+        int V = 5;
+        ArrayList<ArrayList<Integer>> am = new ArrayList<ArrayList<Integer>>(V);
+
+        for (int i = 0; i < V; i++)
+            am.add(new ArrayList<Integer>());
+
+        // Add edges
+        addEdge(am, 0, 1);
+        addEdge(am, 0, 2);
+        addEdge(am, 0, 3);
+        addEdge(am, 1, 2);
+
+        printGraph(am);
+    }
+
 }
